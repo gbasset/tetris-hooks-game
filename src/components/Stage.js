43866533687c05de54d41f2bd144315a9528e218
gaetan <gbasset@protonmail.com>
2020-03-14 17:52:44 +0100

    create the project tetris and the component

---
+++ src/components/Stage.js
@@ -0,0 +1,10 @@
+
+import React, { useState, useEffect } from 'react'
+import Cell from './Cell'
+export default function Stage({ stage }) {
+    return (
+        <div>
+            <Cell />
+        </div>
+    )
+}
